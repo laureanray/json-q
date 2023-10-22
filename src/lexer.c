fb37c6594811b415e09faa1ca5998297b353a5c3
laureanray <laureanraybahala@gmail.com>
2023-10-22 20:21:59 +0800

    try reading file

---
+++ src/lexer.c
@@ -0,0 +1,6 @@
+struct Lexer {
+    char* input;
+    int pos;
+    int read_pos;
+    char ch;
+};
--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
+  FILE *file;
+  char filename[200] = "input.json";
+  file = fopen(filename, "r");
+  if (file) {
+    int chr;
+    while ((chr = fgetc(file)) != EOF)
+      fputc(chr, stdout);
+    fclose(file);
+  } else {
+    printf("File not found.");
+  }
+  printf("\n");
+
+  return 0;
 }
