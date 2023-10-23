81004cfb59229caacf81a4ee61343d8196c2b65f
laureanray <laureanraybahala@gmail.com>
2023-10-23 13:45:22 +0800

    making progress for lexer

--- src/lexer.c
+++ src/lexer.c
@@ -1,6 +1,32 @@
-struct Lexer {
-    char* input;
-    int pos;
-    int read_pos;
-    char ch;
-};
+#include "lexer.h"
+#include <stdlib.h>
+#include <string.h>
+
+static void _lexer_read_char(struct Lexer* lexer);
+
+struct Lexer* lexerCreate(char* input) {
+    int len = sizeof(struct Lexer);
+    struct Lexer* lexer = malloc(len);
+    memset(lexer, 0, len);
+
+    lexer->input = input;
+    lexer->input_length = strlen(input);
+    lexer->read_position = 0;
+    lexer->position = 0;
+
+    _lexer_read_char(lexer);
+
+    return lexer;
+}
+
+
+static void _lexer_read_char(struct Lexer* lexer) {
+    if (lexer->read_position >= lexer->input_length) {
+        lexer->ch = 0;
+    } else {
+        lexer->ch = lexer->input[lexer->read_position];
+    }
+
+    lexer->position = lexer->read_position;
+    lexer->read_position = lexer->read_position + 1;
+}
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "file.h"
+#include "lexer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,4 +8,8 @@ int main(int argc, char *argv[]) {
     char* bufferResult = loadFile("../input.json");
     printf("%zu\n", strlen(bufferResult));
     printf("%s\n", bufferResult);
+    
+    struct Lexer* lexer = lexerCreate(bufferResult);
+
+    printf("%c\n", lexer->ch);
 }
