6451aa4d92e66993fa03d68753e953174017cdc1
laureanray <laureanraybahala@gmail.com>
2023-10-23 13:03:51 +0800

    remove cmakefiles

--- CMakeFiles/jsonq.dir/src/lexer.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/jsonq.dir/src/lexer.c.o: /home/lr/p/json-q/src/lexer.c \
- /usr/include/stdc-predef.h
--- CMakeFiles/jsonq.dir/src/parser.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/jsonq.dir/src/parser.c.o: /home/lr/p/json-q/src/parser.c \
- /usr/include/stdc-predef.h
