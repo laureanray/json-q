8edb46211615042e669b5ae5268ecb3cef19dabd
laureanray <laureanraybahala@gmail.com>
2023-10-30 20:23:55 +0800

    add parser: peek error

--- src/parser.c
+++ src/parser.c
@@ -12,6 +12,7 @@ static struct JsonMember* _parser_parse_json_member(struct Parser *parser);
 static int _parser_expect_peek(struct Parser* parser, TokenType tt);
 static int _parser_peek_token_is(struct Parser* parser, TokenType tt);
 static void _parser_next_token(struct Parser* parser);
+static char* _parser_peek_error(struct Parser* parser, TokenType tt);
 
 static void _parser_next_token(struct Parser *parser) {
     parser->curr_token = parser->peek_token;
@@ -65,8 +66,18 @@ static int _parser_peek_token_is(struct Parser* parser, TokenType tt) {
 static int _parser_expect_peek(struct Parser* parser, TokenType tt) {
     if (_parser_peek_token_is(parser, tt) == 1) {
         _parser_next_token(parser);
+        return 1;
+    } else {
+        _parser_expect_peek(parser, tt);
+        return 0;
     }
 }
 
 
 
+
+static char* _parser_peek_error(struct Parser* parser, TokenType tt) {
+    char* err;
+    sprintf("expected token to be %d got %d instead", err, tt, parser->peek_token->type);
+    return err;
+}
